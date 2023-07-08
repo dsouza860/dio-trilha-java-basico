bae9f79b364a369e2db47e52ba8d3a500b7c3e32
Diego Souza <d.souza860@gmail.com>
2023-07-08 16:24:27 -0300

    implementação do desafio

--- ContaBanco/src/ContaBanco.java
+++ ContaBanco/src/ContaBanco.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ContaBanco {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Informe o número da agência: ");
+        int numAgencia = ler.nextInt();
+        System.out.print("Informe o número da conta: ");
+        String numConta = ler.next();
+        System.out.print("Informe o nome do cliente: ");
+        String nomeCliente = ler.next();
+        ler.nextLine();
+        System.out.print("Saldo: ");
+        double saldo = ler.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua "+
+                "agência é " + numAgencia + ", conta " + numConta + " e seu saldo " + saldo
+                + " já está disponível para saque.");
     }
 }
